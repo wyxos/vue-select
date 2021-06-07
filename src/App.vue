4af331e7312761e3d8741c4582788f1d9f2dc904
Joey J <joey.jaulimsing@live.com>
2021-06-07 22:04:20 +0400

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,103 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Example: Basic</p>
+    <div>
+      <vue-select :options="options" v-model="model"></vue-select>
+    </div>
+
+    <p>Example: Custom option template</p>
+    <div>
+      <vue-select :options="options2" v-model="model2">
+        <template v-slot:label="{option}">
+          {{ option.content.custom }}
+        </template>
+        <template v-slot:option="{option}">
+          {{ option.content.custom }}
+        </template>
+      </vue-select>
+    </div>
+
+    <p>Example: Format v-model output and restore selection from external update</p>
+    <div>
+      <vue-select :options="options3" v-model="model3"
+                  :format-output="(selection) => selection.map(item => item.content.value)"
+                  :external-update="(model, item) => model.indexOf(item.content.value) > -1">
+        <template v-slot:label="{option}">
+          {{ option.content.custom }} - {{ option.isSelected }}
+        </template>
+        <template v-slot:option="{option}">
+          {{ option.content.custom }} - {{ option.isSelected }}
+        </template>
+      </vue-select>
+      {{ model3 }}
+      <button @click="setSelection">Set selection</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueSelect from './components/VueSelect'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VueSelect
+  },
+  data () {
+    return {
+      options: [
+        {
+          label: 'Value 1',
+          value: 'value-1'
+        },
+        {
+          label: 'Value 2',
+          value: 'value-2'
+        },
+        {
+          label: 'Value 3',
+          value: 'value-3'
+        }
+      ],
+      model: [],
+
+      options2: [
+        {
+          custom: 'Value 1',
+          value: 'value-1'
+        },
+        {
+          custom: 'Value 2',
+          value: 'value-2'
+        },
+        {
+          custom: 'Value 3',
+          value: 'value-3'
+        }
+      ],
+      model2: [],
+
+      options3: [
+        {
+          custom: 'Value 1',
+          value: 'value-1'
+        },
+        {
+          custom: 'Value 2',
+          value: 'value-2'
+        },
+        {
+          custom: 'Value 3',
+          value: 'value-3'
+        }
+      ],
+      model3: []
+    }
+  },
+  methods: {
+    setSelection () {
+      this.model3 = ['value-1', 'value-2']
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
